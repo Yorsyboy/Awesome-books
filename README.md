bcabe172ec1dd16ec42be69ef41101659e91f9d0
yorsyboy <adekunletaiwo43@yahoo.com>
2022-04-19 08:36:50 +0100

    JAvascript Objects

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Project Name
 # Awesome-books
+
+## Built With
+- HTML
+- CSS
+- JS
+
+### Prerequisites
+The basic requirements for building the executable are:
+* A working browser application (Google chrome, Mozilla Fire fox, Microsoft edge etc)
+
+### Run tests
+Open index.html in Chrome
+
+## Authors
+
+## Author1
+
+👤 **Toyosi Taiwo**
+
+- GitHub: [@Yorsyboy](https://github.com/Yorsyboy)
+
+- GitHub: [@Yorsyboy](https://github.com/Yorsyboy)
+- Twitter: [@tboytaiwo](https://twitter.com/Tboytaiwo)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/taiwo-toyosi)
+
+
+## Author2
+👤 **Author1**
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
---
+++ main.js
@@ -0,0 +1,85 @@
+// Declare books list
+let books = [];
+
+// Local Storage Functions
+// Check if Storage is available
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      e.code === 22
+      || e.code === 1014
+      || e.name === 'QuotaExceededError'
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      && (storage && storage.length !== 0);
+  }
+}
+
+// Updates the Local Storage
+function updateLocalStorage() {
+  if (storageAvailable('localStorage')) {
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+const localBooksData = localStorage.getItem('books');
+
+// Load initially stored data
+if (localBooksData) {
+  books = JSON.parse(localBooksData);
+}
+
+// Books Values & Functions
+const booksList = document.getElementById('books-list');
+
+// Check if a book exists
+function bookExists(book) {
+  for (let i = 0; i < books.length; i += 1) {
+    if (books[i].title === book.title && books[i].author === book.author) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Remove a book from the list of lists
+function removeBook(book) {
+  for (let i = 0; i < books.length; i += 1) {
+    if (books[i].title === book.title && books[i].author === book.author) {
+      books.splice(i, 1);
+      updateLocalStorage();
+      return;
+    }
+  }
+}
+
+// Shows the added book in html
+function displayNewElement(book) {
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('remove-button');
+  removeButton.textContent = 'Remove';
+
+  bookDiv.innerHTML = `
+    <h2 class="book-title">${book.title}</h2>
+    <p class="book-author">${book.author}</p>
+  `;
+  bookDiv.appendChild(removeButton);
+  bookDiv.appendChild(document.createElement('hr'));
+
+  booksList.appendChild(bookDiv);
+
+  removeButton.addEventListener('click', () => {
+    removeBook(book);
+    bookDiv.remove();
+  });
+}
+
